dc82d1bac7c65f87b846d4b4be91366605c7da6b
xhevat <xhevat.ibraimi@gmail.com>
2022-12-16 03:58:19 +0100

    fixed config bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 ; (() => {
     window.addEventListener('DOMContentLoaded', async () => {
+        window.config = config;
         var app = new App();
         window.app = app;
         await app.initializeAsync();
