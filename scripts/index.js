2e6833d69fe0665c372f0b363ec9bae20e95ddfd
xhevat <xhevat.ibraimi@gmail.com>
2022-12-16 03:49:40 +0100

    fixed config loading

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 ; (() => {
     window.addEventListener('DOMContentLoaded', async () => {
+        window.config = config;
         var app = new App(window.config);
         window.app = app;
         await app.initializeAsync();
